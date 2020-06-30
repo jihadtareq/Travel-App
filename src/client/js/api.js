fb7e16229d42a30be337512a0540102e392bab20
Jihad tarek <jihadtarek60@gmail.com>
2020-06-30 02:49:42 +0200

    getData from geonames API

--- src/client/js/api.js
+++ src/client/js/api.js
@@ -12,3 +12,50 @@ const coordinatesAPI = async(city,country='')=>{
         
     }
 }
+
+//Get DATA from API
+const getDataAPI = async(userData)=>{
+    let trip={}
+
+    trip.error='';
+
+    //get  Current Location
+    await coordinatesAPI(userData.location)
+    .then(resLocation=>{
+        if(resLocation.error){
+            trip.error=resLocation.error;
+        }
+        else{
+            trip.location = {
+                city:resLocation.geonames[0].toponymName,
+                lat: resLocation.geonames[0].lat,
+                lng: resLocation.geonames[0].lng
+            }
+        }
+    })
+
+    //get the destination
+
+    await coordinatesAPI(userData.destination)
+    .then(resDestinaton =>{
+        if(resDestinaton.error){
+            trip.error = resDestinaton.error;
+        }else{
+
+            //add the destination info to the object
+
+            trip.destination = {
+                city: resDestinaton.geonames[0].toponymName,
+                country: resDestinaton.geonames[0].countryName,
+                lat: resDestinaton.geonames[0].lat,
+                lng: resDestinaton.geonames[0].lng
+            };
+        }
+    })
+
+
+}
+export{
+    coordinatesAPI,
+    getDataAPI
+}
