1ea9237baff9f3be9feef46bb89e0df1687701fb
Jihad tarek <jihadtarek60@gmail.com>
2020-06-30 21:11:51 +0200

    edited config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,58 +1,52 @@
 const path = require('path')
-const webpack= require('webpack')
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 module.exports = {
-    entry:'./src/client/index.js',
-    output:{
-      libraryTarget:'var',
-      librart:'Client'
-
+    entry: './src/client/index.js',
+    output: {
+      libraryTarget: 'var',
+      library: 'Client'
     },
-    mode:'development',
-    devtool:'source-map',
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module:{
-        rules:[
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
             {
-                test:'/\.js$',
-                exculde: /node_modules/,
-                loader:"babel-loader"
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
             },
             {
-            test: /\.scss$/,
-            use:[ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
-            },
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            } 
         ]
     },
-    plugins:[
-      new BundleAnalyzerPlugin({
-        analyzerMode:'server',
-    }),
-    new HtmlWebPackPlugin({
-        template: "./src/client/views/index.html",
-        filename: "./index.html",
-    }),
-    new CleanWebpackPlugin({
-        // Simulate the removal of files
-        dry: true,
-        // Write Logs to Console
-        verbose: true,
-        // Automatically remove all unused webpack assets on rebuild
-        cleanStaleWebpackAssets: true,
-        protectWebpackAssets: false
-    }),
-    new WorkboxPlugin.GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true
-  }), 
+    plugins: [
+         new BundleAnalyzerPlugin({
+            analyzerMode:'server',
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new WorkboxPlugin.GenerateSW({
+          clientsClaim: true,
+          skipWaiting: true
+      }), 
     ]
-
-  };
+}
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const OptimizeCSSAssetsPlugin =require('optimize-css-assets-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
