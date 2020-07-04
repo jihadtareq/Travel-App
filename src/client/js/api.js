d3c333d2dd28b9a4a7667c49905a477b4e24b30e
Jihad tarek <jihadtarek60@gmail.com>
2020-07-04 03:51:11 +0200

    get weather and pixaImage data

--- src/client/js/api.js
+++ src/client/js/api.js
@@ -1,3 +1,6 @@
+import { getWeatherAPI } from "../../../../../travel-planner imp/src/client/js/api";
+
+//>>>>>>>>>>>>>Geonames API<<<<<<<<<<<<<<//
 const coordinatesAPI = async(city,country='')=>{
 
     const geonameURL = `http://api.geonames.org/searchJSON?q=${city}&country=${country}&maxRows=10&username=jihadtarek`
@@ -13,7 +16,39 @@ const coordinatesAPI = async(city,country='')=>{
     }
 }
 
-/*>>>>>>>>>>>>>>>****Get DATA from API****<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+//>>>>>>>>>>>>>Weather API<<<<<<<<<<<<<<//
+
+const weatherAPI = async (lat,lng )=>{
+    const APIKEY = '843ec24c64a64247b124dde2c3138298';
+    const forecastURL=`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${APIKEY}`
+    const res = await fetch('https://cors-anywhere.herokuapp.com/' + forecastURL)
+
+    try{
+        const data = await res.json()
+        return data;
+    }catch(error){
+        console.log(error)
+    }
+}
+
+//>>>>>>>>>>>>>Weather API<<<<<<<<<<<<<<//
+const PixaIMG = async (keyword)=>{
+    // (/\s/g) to replace all white space characters  with +
+    keyword = keyword.replace(/\s/g, '+');
+    const APIKEY ='17325397-88f8642b1a8921c4371ea3632'
+    const pixaURL = `https://pixabay.com/api/?key=${APIKEY}&q=${keyword}&image_type=photo&pretty=true`
+    const res = await fetch(pixaURL)
+    try{
+        const data = await res.json()
+        return data
+    }catch(error){
+        console.log(error)
+    }
+
+}
+/*>>>>>>>>>>>>>>>>>>>>>*************************<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+/*>>>>>>>>>>>>>>>>>>>>>****Get DATA from API****<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+/*>>>>>>>>>>>>>>>>>>>>>*************************<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 const getDataAPI = async (userData)=>{
     let trip={}
 
@@ -50,8 +85,46 @@ const getDataAPI = async (userData)=>{
                 lat: resDestinaton.geonames[0].lat,
                 lng: resDestinaton.geonames[0].lng
             };
-            console.log(dataDestination)
+           // console.log(dataDestination)
         }
+    })
+
+    //get weather from
+
+    if(trip.error == ''){
+        await weatherAPI(trip.destination.lat,trip.destination.lng)
+        .then(resweather =>{
+
+           let weather = trip.resweather = {
+                mix_temp : resweather.data[0].max_temp,
+                min_temp : resweather.data[0].min_temp,
+                datetime : resweather.data[0].datetime,
+                timezone : resweather.timezone
+            }
+            console.log(weather)
+        });
+    }
+
+    //get image of destination
+
+    await PixaIMG(trip.destination.city)
+    .then(resCityimag =>{
+       if(resCityimag.totalHits > 0){
+           trip.image = resCityimag.hits[0].largeImageURL
+           console.log( trip.image)
+       }
+       else{
+           PixaIMG(trip.destination.country)
+           .then(resCountryImage => {
+               if(resCountryImage.totalHits > 0){
+                   trip.image = resCountryImage.hits[0].largeImageURL
+               }else{
+                   trip.image = false
+               }
+               console.log( trip.image)
+
+            })
+       }
     })
 
     return trip;
@@ -83,5 +156,7 @@ const saveNewtrip = (newTripHolder) =>{
 }
 export{
     getDataAPI,
-    saveNewtrip
+    saveNewtrip,
+    weatherAPI,
+    PixaIMG
 }
