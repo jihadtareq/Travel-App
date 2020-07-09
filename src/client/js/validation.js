b7fe6504ea99027c9518a88a989c4e1aeb1ff517
Jihad tarek <jihadtarek60@gmail.com>
2020-07-09 17:51:59 +0200

    validate time

--- src/client/js/validation.js
+++ src/client/js/validation.js
@@ -1,8 +1,12 @@
+import { isFutureDate, isDateAfterThatDate,showErrorMessage} from './handler';
+
 const validate = () =>{
     let error = ''
 
     const location = document.getElementById('location')
     const destination = document.getElementById('destination')
+    const dateStart = document.getElementById('date-start')
+    const dateEnd = document.getElementById('date-end')
 
     //check if the current location is valid 
     if(location.value.length < 3)
@@ -29,6 +33,31 @@ const validate = () =>{
         destination.classList.remove('error')
         destination.classList.add('valid')
     }
+     // check the Start Date field
+     if(!isFutureDate(dateStart.value) || dateStart.value == '') {
+
+        dateStart.classList.add('error');
+        dateStart.classList.remove('valid');
+
+        errors += '<p>Please, select the Departing Date <br>(The Date cannot be before today)</p>';
+       
+    } else {
+        //validated
+        dateStart.classList.remove('error');
+        dateStart.classList.add('valid');
+    }
+
+    // check the End Date field
+    if(!isDateAfterThatDate(dateStart.value, dateEnd.value) || !isFutureDate(dateEnd.value) || dateEnd.value == ''){
+        dateEnd.classList.add('error')
+        dateEnd.classList.remove('valid');
+        
+        errors += '<p>Please, select the Returning Date <br>(The Date cannot be before the Departing Date)</p>';
+        
+    } else {
+        dateEnd.classList.remove('error')
+        dateEnd.classList.add('valid');
+    }
 
     //in case there is no error 
     if(!error){
@@ -36,11 +65,15 @@ const validate = () =>{
         //return the validated object
         return {
             location:location.value,
-            destination:destination.value
+            destination:destination.value,
+            dateStart: dateStart.value, 
+            dateEnd: dateEnd.value
+
         }
     }else{
         document.getElementById('search').classList.remove('disabled')
-        alert('Can not Search')
+       // alert('Can not Search')
+        showErrorMessage(errors);
         return false
     }
 }
