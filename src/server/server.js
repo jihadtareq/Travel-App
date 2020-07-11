fe59b4e16d49143fcebba9d61af06eef967cf170
Jihad tarek <jihadtarek60@gmail.com>
2020-07-11 02:14:30 +0200

    sum fun

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,3 @@
-// Setup Server// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of app
@@ -22,9 +19,14 @@ app.use(express.static('dist'));
 
 const port = 8080;
 
-const listen = app.listen(port,listening);
+app.listen(port, async () => {
+    console.log('The server is runing on http://localhost:8080');
+});
 
-function listening(){
-    console.log("#running server");
-    console.log(`running on localhost: ${port}`);
-}
+
+
+function sum(a, b) {
+    return a + b;
+  }
+
+ module.exports = sum;
